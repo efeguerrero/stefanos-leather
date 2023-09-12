0a545b2ba7dd8d2e2096cea9e99ef695a01d1436
efeguerrero <juanfg.8640@gmail.com>
2023-09-12 16:32:50 -0300

    style[filter]: Radix accordion implemented

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -10,7 +10,6 @@ export default function ProductGrid({ products }: ProductGridProps) {
   return (
     <div className="bg-white">
       <h2 className="sr-only">Products</h2>
-
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {products.map((product: any) => (
           <ProductCard key={product.sys.id} product={product} />
--- src/components/productsLayout/Filters.tsx
+++ src/components/productsLayout/Filters.tsx
@@ -5,6 +5,9 @@ import { Disclosure, Transition } from "@headlessui/react";
 
 import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
 
+//Radix Imports
+import * as Accordion from "@radix-ui/react-accordion";
+
 interface FiltersProps {
   filterOptions: {
     name: string;
@@ -24,7 +27,7 @@ const Filters = ({
       <h3 className="sr-only">Categories</h3>
       <ul
         role="list"
-        className="border-gray-300 space-y-4 border-b pb-6 text-sm font-medium"
+        className="space-y-4 border-b border-gray-300 pb-6 text-sm font-medium"
       >
         {filterOptions.map((filterOption, index) => (
           <li key={index}>
@@ -33,61 +36,50 @@ const Filters = ({
         ))}
       </ul>
 
-      {categories.map((category: any) => (
-        <Disclosure
-          as="div"
+      {categories.map((category: any, index: number) => (
+        <Accordion.Root
+          asChild
+          type="multiple"
           key={category.sys.id}
-          className="border-gray-300 border-b py-6"
+          className="border-b border-gray-300 py-6"
         >
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="text-gray-500 hover:text-gray-600 flex w-full items-center justify-between bg-white text-sm">
+          <Accordion.Item value={`menu-${index}`}>
+            <Accordion.Header className="group">
+              <Accordion.Trigger className="flex w-full items-center justify-between bg-white text-sm text-gray-500">
                 <h3 className="font-medium text-black ">
                   {category.fields.name}
                 </h3>
-                <span className="ml-6 flex items-center">
-                  {open ? (
-                    <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                  ) : (
-                    <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                  )}
-                </span>
-              </Disclosure.Button>
-              <Transition
-                enter="transition duration-200 ease-out"
-                enterFrom="transform opacity-0"
-                enterTo="transform  opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform opacity-100"
-                leaveTo="transform opacity-0"
-              >
-                <Disclosure.Panel className="pt-6" static>
-                  <div className="flex flex-col gap-2">
-                    <Link
-                      href={`/products/${category.fields.slug}`}
-                      className="text-gray-600 text-sm"
-                    >{`All ${category.fields.name} `}</Link>
-                    {subCategories.map((subCategory: any) => {
-                      return (
-                        <Fragment key={subCategory.sys.id}>
-                          {category.fields.slug ===
-                          subCategory.fields.category.fields.slug ? (
-                            <Link
-                              href={`/products/${category.fields.slug}/${subCategory.fields.slug}`}
-                              className="text-gray-600 text-sm"
-                            >
-                              {subCategory.fields.name}
-                            </Link>
-                          ) : null}
-                        </Fragment>
-                      );
-                    })}
-                  </div>
-                </Disclosure.Panel>
-              </Transition>
-            </>
-          )}
-        </Disclosure>
+                <div className="relative ml-6 flex h-5 w-5 items-center">
+                  <MinusIcon className="absolute hidden h-full w-full group-data-[state=open]:block " />
+                  <PlusIcon className="absolute h-full w-full group-data-[state=open]:hidden" />
+                </div>
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="pt-6">
+              <div className="flex flex-col gap-2">
+                <Link
+                  href={`/products/${category.fields.slug}`}
+                  className="text-sm text-gray-600"
+                >{`All ${category.fields.name} `}</Link>
+                {subCategories.map((subCategory: any) => {
+                  return (
+                    <Fragment key={subCategory.sys.id}>
+                      {category.fields.slug ===
+                      subCategory.fields.category.fields.slug ? (
+                        <Link
+                          href={`/products/${category.fields.slug}/${subCategory.fields.slug}`}
+                          className="text-sm text-gray-600"
+                        >
+                          {subCategory.fields.name}
+                        </Link>
+                      ) : null}
+                    </Fragment>
+                  );
+                })}
+              </div>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
       ))}
     </>
   );
