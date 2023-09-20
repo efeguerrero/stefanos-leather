cc7981b7a532e9afbc95a45e67a787df1aa1bf97
efeguerrero <juanfg.8640@gmail.com>
2023-09-20 16:16:07 -0300

    optimization[Card]: Sizes for Image Component

--- src/components/categories/Card.tsx
+++ src/components/categories/Card.tsx
@@ -31,6 +31,7 @@ const Card = ({ category }: CardProps) => {
           src={`https:${coverImageUrl}`}
           alt={`Cover image for ${name}`}
           fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="h-full w-full object-cover object-center"
         />
       </div>
