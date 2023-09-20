e38cc672ef68f737229a972aa30171d5a85270ba
efeguerrero <juanfg.8640@gmail.com>
2023-09-20 16:40:26 -0300

    logo now has defined widht and height

--- src/components/nav/Logo.tsx
+++ src/components/nav/Logo.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 //Next Imports
 import Link from "next/link";
-
-//Framer Motion Imports
-import { motion } from "framer-motion";
+import Image from "next/image";
 
 //Assets Imports
 import logo from "/public/assets/images/brand/logo.png";
@@ -12,7 +10,13 @@ import logo from "/public/assets/images/brand/logo.png";
 const Logo = () => {
   return (
     <Link href="/">
-      <motion.img src={logo.src} alt="Brand Logo" className="h-8 w-auto" />
+      <Image
+        src={logo.src}
+        width={462}
+        height={101}
+        alt="Brand Logo"
+        className="h-8 w-auto"
+      />
     </Link>
   );
 };
