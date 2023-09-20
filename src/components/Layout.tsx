da0d3ce8d438a3a1b305320e70e4020297bc4a63
efeguerrero <juanfg.8640@gmail.com>
2023-09-20 13:35:12 -0300

    meta data

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,31 @@ const Layout = ({ children }: LayoutProps) => {
       <section className="flex min-h-[100dvh] flex-col">
         <Head>
           <title>Stefanos Leather</title>
+          {/* <!-- Meta Tags --> */}
+          <meta name="description" content="Argentine Leather To The World" />
+          <meta
+            name="keywords"
+            content="leather, cow, sheep, cotton, rug, handbag, pillow, raw, import, export , argentina, lamb, bkf, hat, commerce, shop, buenos aires, stefanos, stefanos leather, tote, handcraft"
+          />
+          <meta property="og:site_name" content="Stefanos Leather" />
+          <meta property="og:title" content="Stefanos Leather" />
+          <meta
+            property="og:description"
+            content="Argentine Leather To The World"
+          />
+          <meta
+            property="og:image"
+            content="https://leather-preview.vercel.app/assets/images/brand/ogImage.png"
+          />
+          <meta property="og:image:type" content="png" />
+          <meta property="og:image:width" content="50" />
+
+          <meta
+            name="twitter:image"
+            content="https://leather-preview.vercel.app/assets/images/brand/ogImage.png"
+          />
+          <meta name="twitter:image:type" content="png" />
+          <meta name="twitter:image:width" content="50" />
         </Head>
         <Nav />
         <main>{children}</main>
